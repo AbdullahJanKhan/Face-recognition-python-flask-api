e1bcf476afcf1395e2f812e3032bc3c28e5b560b
AbdullahJanKhan <abdullah.jankhan445@hotmail.com>
2021-12-04 17:33:09 +0500

    FR Api updated

--- FR.py
+++ FR.py
@@ -66,8 +66,8 @@ api.add_resource(isHuman, "/is_human")
 
 class faceRecognition(Resource):
     def post(self):
-        data = json.loads(request.data)
-        if 'file' not in data:
+        # data = json.loads(request.data)
+        if 'file' not in request.files:
             return jsonify({
                 "status": False,
                 "fileuploade": False
@@ -78,16 +78,16 @@ class faceRecognition(Resource):
         final = ""
         encoding_1 = 0
         encoding_2 = 0
-        filesEncoded = data['file']
-        filesDecoded = [base64.b64decode(f) for f in filesEncoded]
+        # filesEncoded = data['file']
+        # filesDecoded = [base64.b64decode(f) for f in filesEncoded]
         j = 0
-        for file in filesDecoded:
-            with open('file'+str(j)+'.jpg', 'wb') as f:
-                f.write(file)
-                j += 1
+        # for file in filesDecoded:
+        #     with open('file'+str(j)+'.jpg', 'wb') as f:
+        #         f.write(file)
+        #         j += 1
         filesDecoded = os.listdir('./')
-        for file in filesDecoded:
-            if file.endswith('.jpg'):
+        for file in request.files.getlist('file'):
+            if file and allowed_file(file.filename):
                 if i == 0:
                     known_image = face_recognition.load_image_file(file)
                     encoding_1 = face_recognition.face_encodings(known_image)[
@@ -98,20 +98,13 @@ class faceRecognition(Resource):
                     encoding_2 = face_recognition.face_encodings(u_image)[0]
                     break
         final = ""
-        results = face_recognition.compare_faces([encoding_1], encoding_2)
-        if results[0]:
-            final = "matched"
-        else:
-            final = "Not matched"
-        for f in filesDecoded:
-            if f.endswith('.jpg'):
-                os.remove('./'+f)
+        results = face_recognition.compare_faces(
+            [encoding_1], encoding_2, tolerance=0.85)
         return jsonify({
-            "output": final
+            "output": str(results[0])
         })
-
 
 api.add_resource(faceRecognition, "/is_sameperson")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True)
--- sameperson.py
+++ sameperson.py
@@ -9,13 +9,15 @@ from flask import Flask, redirect
 from flask_restful import Api, Resource
 # from requests.models import Responsepip
 import os
+from PIL import Image
+
 app = Flask(__name__)
 api = Api(app)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 path = os.getcwd()
 # file Upload
 UPLOAD_FOLDER = os.path.join(path, 'uploads')
-final=""
+final = ""
 
 if not os.path.isdir(UPLOAD_FOLDER):
     os.mkdir(UPLOAD_FOLDER)
@@ -30,48 +32,48 @@ def allowed_file(filename):
 
 class getextension2(Resource):
     def post(self):
-        
-        
+        print(request.files)
         if 'file' not in request.files:
             return ({
                 "status": False,
                 "fileuploade": False
-
             })
-         
-        i=0
-        final=""
-        encoding_1=0;
-        encoding_2=0;
+        i = 0
+        final = ""
+        encoding_1 = 0
+        encoding_2 = 0
         for file in request.files.getlist('file'):
             if file and allowed_file(file.filename):
-
-                if i==0:
+                file.save(file.filename)
+                if i == 0:
+                    # f = Image.open(file.filename)
                     known_image = face_recognition.load_image_file(file)
-                    encoding_1 = face_recognition.face_encodings(known_image)[0]
-                    i=i+1
+                    encoding_1 = face_recognition.face_encodings(known_image)[
+                        0]
+                    i = i+1
                 else:
-                     u_image = face_recognition.load_image_file(file)
-                     encoding_2 = face_recognition.face_encodings(u_image)[0]
-                     break
-                     
-                    
-
+                    # f = Image.open(file.filename)
+                    u_image = face_recognition.load_image_file(file)
+                    encoding_2 = face_recognition.face_encodings(u_image)[
+                        0]
+                    break
             else:
                 return({
-                        "allowedextensionsare": " png, jpg, jpeg"
+                    "allowedextensionsare": " png, jpg, jpeg"
                 })
-        final=""
+        final = ""
         results = face_recognition.compare_faces([encoding_1], encoding_2)
+        print(results)
         if results[0]:
-            final="matched"
+            final = "matched"
         else:
-            final="Not matched"        
+            final = "Not matched"
+
         return({
-            "output":final
+            "output": final
         })
 
-        
+
 api.add_resource(getextension2, "/is_sameperson")
 
 if __name__ == "__main__":
